5c770e2fe7848ff50941dc4bca230df0161cd42c
saraytin <tin19003@byui.edu>
2021-01-23 10:19:21 -0600

    week3 commit

---
+++ lesson3/js/lastModified.js
@@ -0,0 +1,3 @@
+var lastModified = document.lastModified;
+document.getElementById("date").innerHTML = "Last Updated on " + lastModified;
+document.getElementById("year").innerHTML = new Date().getFullYear();
